d95ffe80fde0d3a03d5957c9ad6ff251acae8638
Grant Orchard <go@hashicorp.com>
2020-09-17 20:25:51 +1000

    initial commit for 1 node SDDC

--- main.tf
+++ main.tf
@@ -1,1 +1,44 @@
+locals {
+  aws_account_number = data.terraform_remote_state.aws-core.outputs.account_id
+  public_subnets = data.terraform_remote_state.aws-core.outputs.public_subnets
+  #vpc_cidr = data.terraform_remote_state.aws-core.outputs.vpc_cidr
+}
+
+provider "vmc" {
+  refresh_token = var.refresh_token
+}
+
+data vmc_connected_accounts "this" {
+  account_number = local.aws_account_number
+}
+
+data vmc_customer_subnets "this" {
+  connected_account_id = data.vmc_connected_accounts.this.id
+  region               = var.region
+}
+
+resource "vmc_sddc" "sddc_1" {
+  sddc_name           = var.sddc_name
+  vpc_cidr            = var.vpc_cidr
+  num_host            = var.sddc_num_hosts
+  provider_type       = "AWS"
+  region              = var.region
+  vxlan_subnet        = var.vxlan_subnet
+  delay_account_link  = false
+  skip_creating_vxlan = false
+  sso_domain          = "vmc.local"
+  host_instance_type  = "I3_METAL"
+  sddc_type           = "1NODE"
+
+  deployment_type = "SingleAZ"
+  account_link_sddc_config {
+    customer_subnet_ids  = local.public_subnets
+    connected_account_id = local.aws_account_number
+  }
+  timeouts {
+    create = "300m"
+    update = "300m"
+    delete = "180m"
+  }
+}
 
--- variables.tf
+++ variables.tf
@@ -1,1 +1,31 @@
+variable refresh_token {
+  type = string
+}
+/*variable org_id {
+  type = string
+}
+*/
+variable region {
+  type = string
+  default = "ap-southeast-2"
+}
 
+variable sddc_name {
+  type = string
+  default = "VMCasCode"
+}
+
+variable vpc_cidr {
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable sddc_num_hosts {
+  type = number
+  default = 1
+}
+
+variable vxlan_subnet {
+  type = string
+  default = "172.16.1.0/24"
+}
