b58e24b88189c87f06d33dc6861e6d30346d0050
Grant Orchard <go@hashicorp.com>
2020-09-18 13:57:03 +1000

    updated AWS account mapping to use internal VMC id

--- main.tf
+++ main.tf
@@ -6,34 +6,38 @@ locals {
 
 provider "vmc" {
   refresh_token = var.refresh_token
+  org_id = var.org_id
 }
+
 
 data vmc_connected_accounts "this" {
   account_number = local.aws_account_number
 }
 
+/*
 data vmc_customer_subnets "this" {
   connected_account_id = data.vmc_connected_accounts.this.id
   region               = var.region
 }
+*/
 
 resource "vmc_sddc" "this" {
   sddc_name           = var.sddc_name
-  vpc_cidr            = var.vpc_cidr
+  vpc_cidr            = "10.2.0.0/16"
   num_host            = var.sddc_num_hosts
-  provider_type       = "AWS"
-  region              = upper(var.region)
-  vxlan_subnet        = var.vxlan_subnet
-  delay_account_link  = false
-  skip_creating_vxlan = false
+  #sddc_type           = "1Node"
+  #provider_type       = "AWS"
+  region              = var.region
+  vxlan_subnet        = "10.10.10.0/23"
+  delay_account_link  = true
+  skip_creating_vxlan = true
   sso_domain          = "vmc.local"
   host_instance_type  = "I3_METAL"
-  sddc_type           = "1NODE"
 
   deployment_type = "SingleAZ"
   account_link_sddc_config {
     customer_subnet_ids  = [local.public_subnets[0]]
-    connected_account_id = local.aws_account_number
+    connected_account_id = data.vmc_connected_accounts.this.id
   }
   timeouts {
     create = "300m"
